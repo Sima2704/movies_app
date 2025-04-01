fb0f2c48b6be9780b5cd4b2a1a3814af5d72728c
Sima2704 <128591054+Sima2704@users.noreply.github.com.>
2025-04-01 18:28:18 +0300

    Structure and arrange components on the details page

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { DetailsActorsComponent } from "../details-actors/details-actors.component";
+import { DetailsReviewsComponent } from "../details-reviews/details-reviews.component";
+import { DetailsMoviesComponent } from "../details-movies/details-movies.component";
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [DetailsActorsComponent, DetailsReviewsComponent, DetailsMoviesComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
 
+  
 }
