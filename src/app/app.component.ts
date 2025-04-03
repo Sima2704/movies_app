8df1ed68ca00a19bfaf55d5a84c0ff598072f481
Sima2704 <128591054+Sima2704@users.noreply.github.com.>
2025-04-03 14:16:02 +0300

    Import all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DetailsComponent} from './components/details/details.component';
+import { DetailsMoviesComponent } from './components/details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './components/details-reviews/details-reviews.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotAuthorizedComponent } from './components/not-authorized/not-authorized.component';
+import { SearchComponent } from './components/search/search.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ DetailsComponent, DetailsMoviesComponent, DetailsReviewsComponent, FooterComponent, HomeComponent, 
+    LoaderComponent, ModalComponent, NavbarComponent, NotAuthorizedComponent, SearchComponent,
+   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'movie_app';
+  
+  
 }
